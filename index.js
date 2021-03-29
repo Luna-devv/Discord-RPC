a53ea0febb7f35272479d863bc0594aee1f4ccf6
Morris‘s recovery partition <71079641+MorrisSeemann@users.noreply.github.com>
2021-03-29 20:32:49 +0200

    added commends

--- index.js
+++ index.js
@@ -7,21 +7,22 @@ client.on('ready', () => {
     console.log("|                                             |");
     console.log("|           Das DRP ist nun online!           |");
     console.log("|                                             |");
-    console.log("|_____________________________________________|");
+    console.log("|_____________________________________________|");     //Log output
 
 
     client.request('SET_ACTIVITY', {
         pid: process.pid,
         activity : {
-        details : "from Microsoft",
+        details : "from Microsoft",                 //description
         assets : {
-            large_image : "big",
-            small_image : "small",
-            large_text : "Discord is in Danger!",
-            small_text : "Logo"
+            large_image : "big",                    //large Picture
+            small_image : "small",                  //small Picture
+            large_text : "Discord is in Danger!",   //large Picture text (if u hover over it)
+            small_text : "Logo"                     //small Picture text (if u hover over it)
         },
-        buttons : [{label : "SaveDiscord - Website", url : "http://bit.ly/SaveDiscord_Web"},{label : "SaveDiscord - Discord",url : "http://bit.ly/SaveDiscord_Dc"}]
+        buttons : [{label : "SaveDiscord - Website", url : "http://bit.ly/SaveDiscord_Web"}, //First Button
+                   {label : "SaveDiscord - Discord",url : "http://bit.ly/SaveDiscord_Dc"}]  //Second Button
     }
     })
 })
-client.login({ clientId : "825853950833328201" }).catch(console.error);
+client.login({ clientId : "825853950833328201" }).catch(console.error); //ClientID
