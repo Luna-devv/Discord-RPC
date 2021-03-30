d8d550c27abbf7a758da7e3c9a3109a0ea0b90e1
Morris‘s recovery partition <71079641+MorrisSeemann@users.noreply.github.com>
2021-03-30 14:19:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,17 +16,17 @@ client.on('ready', () => {
     client.request('SET_ACTIVITY', {
         pid: process.pid,
         activity : {
-        details : "from Microsoft",                 //description
-        assets : {
-            large_image : "big",                    //large Picture
-            small_image : "small",                  //small Picture
-            large_text : "Discord is in Danger!",   //large Picture text (if u hover over it)
-            small_text : "Logo"                     //small Picture text (if u hover over it)
+            details : "from Microsoft",                 //description
+            assets : {
+                large_image : "big",                    //large Picture
+                small_image : "small",                  //small Picture
+                large_text : "Discord is in Danger!",   //large Picture text (if u hover over it)
+                small_text : "Logo"                     //small Picture text (if u hover over it)
+            },
 
-        },
-        buttons : [{label : "SaveDiscord - Website", url : "http://bit.ly/SaveDiscord_Web"}, //First Button
-                   {label : "SaveDiscord - Discord",url : "http://bit.ly/SaveDiscord_DC"}]  //Second Button
-    }
+            buttons : [{label : "SaveDiscord - Website", url : "http://bit.ly/SaveDiscord_Web"}, //First Button
+                       {label : "SaveDiscord - Discord",url : "http://bit.ly/SaveDiscord_DC"}]  //Second Button
+        }
     })
 })
 client.login({ clientId : "825853950833328201" }).catch(console.error); //ClientID
