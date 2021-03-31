9af5b5da38be08be0c5fa395698bd17748142352
Morris‘s recovery partition <71079641+MorrisSeemann@users.noreply.github.com>
2021-03-31 14:47:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const updatePresence = async () => {
                 small_text : "SaveDiscord.com"              //small Picture text (if u hover over it)
             },
             buttons : [
-                {label : "SaveDiscord - Website" , url : "https://savediscord.com"},        //First Button
-                {label : "SaveDiscord - Discord",url : "https://discord.gg/FRA3JQ6MHy"}     //Second Button
+                {label : "SaveDiscord - Website" , url : "https://bit.ly/SaveDiscord_Web"},          //First Button
+                {label : "SaveDiscord - Discord",url : "https://bit.ly/SaveDiscord_Dc"}              //Second Button
             ]
         }
     })
