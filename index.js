8f879cba82bb849a63be4c21b0db56e4c268f99b
Morris‘s recovery partition <71079641+MorrisSeemann@users.noreply.github.com>
2021-03-31 14:38:23 +0200

    Live User Counter

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
-var rpc = require("discord-rpc")                        //import discord-rpc
-const client = new rpc.Client({ transport: 'ipc' })     //define "client"
-var users = "700+";                                     //define "users"
+var rpc = require("discord-rpc")                            //import discord-rpc
+const client = new rpc.Client({ transport: 'ipc' })         //define "client"
+const fetch = require("node-fetch")                         //define "fetch"
+
+
+
+let users = "A lot"                                         //define "users" (Placeholder)
+
+const updatePresence = async () => {
+
+    fetch("https://savediscord.com/api/usercount", { method: "GET" })       //Get Users
+    .then(res => res.text())
+    .then(body => users = body);
+
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity : {
+            details : "from Microsoft",                     //description
+            state: users+" Users registred",                //How much Users
+            assets : {
+                large_image : "big",                        //large Picture
+                small_image : "small",                      //small Picture
+                large_text : "Discord is in Danger!",       //large Picture text (if u hover over it)
+                small_text : "SaveDiscord.com"              //small Picture text (if u hover over it)
+            },
+            buttons : [
+                {label : "SaveDiscord - Website" , url : "https://savediscord.com"},        //First Button
+                {label : "SaveDiscord - Discord",url : "https://discord.gg/FRA3JQ6MHy"}     //Second Button
+            ]
+        }
+    })
+}
+
 
 client.on('ready', () => {
     console.log(" __________________________________________________________________ ");
@@ -9,29 +39,10 @@ client.on('ready', () => {
     console.log("|             The Discord Rich-Presence is now online              |");
     console.log("|                                                                  |");
     console.log("|                                                                  |");
-    console.log("|                     <From: Morris & DreiMu>                      |");
+    console.log("|                     <From: Morris & LPTP1>                       |");
     console.log("|__________________________________________________________________|");     //Log output
 
-
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity : {
-            details : "from Microsoft",				    //description
-            
-            state: users+" User registred",          	//How much Users
-	                "party": {"id": "#savediscord"},   	//PartyID
-            assets : {
-                large_image : "big",                    //large Picture
-                small_image : "small",                  //small Picture
-                large_text : "Discord is in Danger!",   //large Picture text (if u hover over it)
-                small_text : "Logo"                     //small Picture text (if u hover over it)
-            },
-
-            buttons : [
-		    {label : "SaveDiscord - Website", url : "http://bit.ly/SaveDiscord_Web"},	       //First Button
-            {label : "SaveDiscord - Discord",url : "http://bit.ly/SaveDiscord_DC"}      //Second Button
-	    ]
-        }
-    })
+    setInterval(updatePresence, 5000)                                       //Update every 5 seconds
+    
 })
-client.login({ clientId : "825853950833328201" }).catch(console.error); //ClientID
+client.login({ clientId : "825853950833328201" }).catch(console.error);     //ClientID
