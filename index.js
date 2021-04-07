85339336f0fd19d9bd7b7005e3155007c22c1ba9
alicagirllol <56559528+alicagirllol@users.noreply.github.com>
2021-04-07 20:50:50 +0300

    faster code brr

--- index.js
+++ index.js
@@ -1,48 +1,7 @@
-var rpc = require("discord-rpc")                            //import discord-rpc
-const client = new rpc.Client({ transport: 'ipc' })         //define "client"
-const fetch = require("node-fetch")                         //define "fetch"
-
-
-
-let users = "A lot"                                         //define "users" (Placeholder)
-
-const updatePresence = async () => {
-
-    fetch("https://savediscord.com/api/usercount", { method: "GET" })       //Get Users
-    .then(res => res.text())
-    .then(body => users = body);
-
-    client.request('SET_ACTIVITY', {
-        pid: process.pid,
-        activity : {
-            details : "from Microsoft",                     //description
-            state: users+" Users registred",                //How much Users
-            assets : {
-                large_image : "big",                        //large Picture
-                small_image : "small",                      //small Picture
-                large_text : "Discord is in Danger!",       //large Picture text (if u hover over it)
-                small_text : "SaveDiscord.com"              //small Picture text (if u hover over it)
-            },
-            buttons : [
-                {label : "SaveDiscord - Website" , url : "https://bit.ly/SaveDiscord_Web"},          //First Button
-                {label : "SaveDiscord - Discord",url : "https://bit.ly/SaveDiscord_Dc"}              //Second Button
-            ]
-        }
-    })
-}
-
-
-client.on('ready', () => {
-    console.log(" __________________________________________________________________ ");
-    console.log("|                                                                  |");
-    console.log("|                                                                  |");
-    console.log("|             The Discord Rich-Presence is now online              |");
-    console.log("|                                                                  |");
-    console.log("|                                                                  |");
-    console.log("|                     <From: Morris & LPTP1>                       |");
-    console.log("|__________________________________________________________________|");     //Log output
-
-    setInterval(updatePresence, 5000)                                       //Update every 5 seconds
-    
-})
-client.login({ clientId : "825853950833328201" }).catch(console.error);     //ClientID
+var rpc = require("discord-rpc")                            
+const client = new rpc.Client({ transport: 'ipc' })         
+const fetch = require("node-fetch")                         
+let users = "A lot of"                                         
+const updatePresence = async () => {fetch("https://savediscord.com/api/usercount", { method: "GET" }).then(res => res.text()).then(body => users = body);client.request('SET_ACTIVITY', {pid: process.pid,activity : {details : "from Microsoft",state: users + " Users registred",assets : {large_image : "big",small_image : "small",large_text : "Discord is in Danger!",small_text : "SaveDiscord.com"},buttons : [{label : "SaveDiscord - Website" , url : "https://bit.ly/SaveDiscord_Web"},{label : "SaveDiscord - Discord",url : "https://bit.ly/SaveDiscord_Dc"}]}})}
+client.on('ready', () => {console.log(" __________________________________________________________________ ");console.log("|                                                                  |");console.log("|                                                                  |");console.log("|             The Discord Rich-Presence is now online              |");console.log("|                                                                  |");console.log("|                                                                  |");console.log("|                     <From: Morris & LPTP1>                       |");console.log("|__________________________________________________________________|"); setTimeout(updatePresence, 5000);setInterval(updatePresence, 60000)});
+client.login({ clientId : "825853950833328201" }).catch(console.error);
